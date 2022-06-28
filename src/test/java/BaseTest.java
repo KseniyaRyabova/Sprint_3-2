036be1330ae031273f32cc4060f5e1a65c79e46e
Kseniya <ryabova.ksyusha@gmail.com>
2022-06-28 22:20:07 +0300

    correction BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,14 @@
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class BaseTest {
-    @Before
+    @BeforeClass
     public void setUp() {
-        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
+        RequestSpecification specification = RestAssured.given();
+        specification.baseUri("https://qa-scooter.praktikum-services.ru");
+        specification.header("Content-type", "application/json");
     }
 
 }
