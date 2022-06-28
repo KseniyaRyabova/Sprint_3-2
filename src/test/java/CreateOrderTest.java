2bce025c0ad99529d5594b85a30e6e50d45e4c9f
Kseniya <ryabova.ksyusha@gmail.com>
2022-06-28 23:25:30 +0300

    add class variables

--- src/test/java/CreateOrderTest.java
+++ src/test/java/CreateOrderTest.java
@@ -2,12 +2,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.List;
+
+import static io.restassured.RestAssured.filters;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(Parameterized.class)
 public class CreateOrderTest extends BaseTest{
-    private OrderRequest orderRequest;
+    OrderRequest orderRequest;
+    private static final String firstName = "firstName";
+    private static final String lastName = "lastName";
+    private static final String address = "Konoha";
+    private static final int metroStation = 4;
+    private static final String phone = "+7 800 355 35 35";
+    private static final int rentTime = 4;
+    private static final String deliveryDate = "2020-06-06";
+    private static final String comment = "comment";
 
     public CreateOrderTest(OrderRequest orderRequest) {
         this.orderRequest = orderRequest;
@@ -16,18 +27,18 @@ public class CreateOrderTest extends BaseTest{
     @Parameterized.Parameters
     public static Object[] getData() {
         return new Object[][] {
-                {new OrderRequest("firstName", "Naruto", "Konoha, 142 apt.",
-                        4, "+7 800 355 35 35", 4, "2020-06-06",
-                        "comment", new String[]{"BLACK"})},
-                {new OrderRequest("firstName", "Naruto", "Konoha, 142 apt.",
-                        4, "+7 800 355 35 35", 4, "2020-06-06",
-                        "comment", new String[]{"BLACK, GRAY"})},
-                {new OrderRequest("firstName", "Naruto", "Konoha, 142 apt.",
-                        4, "+7 800 355 35 35", 4, "2020-06-06",
-                        "comment", new String[]{"GRAY"})},
-                {new OrderRequest("firstName", "Naruto", "Konoha, 142 apt.",
-                        4, "+7 800 355 35 35", 4, "2020-06-06",
-                        "comment", null)},
+                {new OrderRequest(firstName, lastName, address,
+                        metroStation, phone, rentTime, deliveryDate,
+                        comment, new String[]{"BLACK"})},
+                {new OrderRequest(firstName, lastName, address,
+                        metroStation, phone, rentTime, deliveryDate,
+                        comment, new String[]{"BLACK, GRAY"})},
+                {new OrderRequest(firstName, lastName, address,
+                        metroStation, phone, rentTime, deliveryDate,
+                        comment, new String[]{"GRAY"})},
+                {new OrderRequest(firstName, lastName, address,
+                        metroStation, phone, rentTime, deliveryDate,
+                        comment, null)},
         };
     }
 
